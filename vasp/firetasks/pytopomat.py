df2cc1a6e03309362f71f754c6ceebf42efd3f57
jeng-yuantsai <tsaie79@gmail.com>
2021-06-16 13:49:37 -0400

    update

--- vasp/firetasks/pytopomat.py
+++ vasp/firetasks/pytopomat.py
@@ -24,7 +24,7 @@ from pytopomat.vasp2trace_caller import (
 from pytopomat.z2pack_caller import Z2PackCaller
 
 from fireworks import explicit_serialize, FiretaskBase, FWAction
-from fireworks import DATETIME_HANDLER
+from fireworks.utilities.fw_serializers import DATETIME_HANDLER
 
 from atomate.utils.utils import env_chk, get_logger
 from atomate.utils.database import CalcDb
