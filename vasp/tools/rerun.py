37bee951245146ab238a7cb5e24b068e854cc55a
jeng-yuantsai <tsaie79@gmail.com>
2021-06-26 21:28:38 -0400

    update

--- vasp/tools/rerun.py
+++ vasp/tools/rerun.py
@@ -15,3 +15,20 @@ def rerun_irvsp(fw_id):
     lpad.update_spec([fw_id], ff.as_dict()["spec"])
     os.chdir(p)
     subprocess.call("rlaunch -c /global/homes/t/tsaie79/config/project/C2DB_IR/calc_data/ singleshot -f {}".format(fw_id).split(" "))
+
+def rerun_relax():
+    from fireworks import LaunchPad
+    import os
+    lpad = LaunchPad.from_file(
+        os.path.join(os.path.expanduser("~"), "config/project/Scan2dMat/calc_data/my_launchpad.yaml"))
+
+    for fw_id in lpad.get_fw_ids(
+            {"state": "RUNNING", "name": {"$regex":"SCAN_relax lc"}, "spec._fworker": {"$in": ["owls", "efrc"]}}
+    ):
+        print(fw_id)
+
+if __name__ == '__main__':
+    rerun_relax()
+
+
+
