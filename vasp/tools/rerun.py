fe09b332b42e98714358eed407e8dedf68ee96ec
jeng-yuantsai <tsaie79@gmail.com>
2021-09-21 10:47:48 -0400

    update

--- vasp/tools/rerun.py
+++ vasp/tools/rerun.py
@@ -19,57 +19,68 @@ def rerun_irvsp(fw_id):
 
 
 #%%
-def rerun_relax(fworker):
+def rerun_fw(fworker):
     """
     fworker = "nersc", "owls", "efrc"
     """
     from fireworks import LaunchPad
-    import os, subprocess, shutil
+    import os, subprocess, shutil, datetime
     from glob import glob
-    from pymatgen.io.vasp.inputs import Structure
+    from pymatgen.io.vasp.inputs import Structure, Incar
     from pymatgen.io.vasp.outputs import Oszicar
+    from qubitPack.tool_box import get_db
 
-    lpad = LaunchPad.from_file(
-        os.path.join(os.path.expanduser("~"), "config/project/Scan2dMat/calc_data/my_launchpad.yaml"))
+    db = get_db("mgb2", "workflows", port=12345, user="Jeng")
+    lpad = LaunchPad(host=db.host, port=db.port, name=db.db_name, username=db.user, password=db.password)
 
     def delet_dir(dir_name):
         shutil.rmtree(dir_name)
     def rerun_opt(fw_id): #421
         # for f in glob("POSCAR*"):
         #     os.remove(f)
-        lpad.rerun_fw(fw_id)
+        lpad.rerun_fw(fw_id, recover_launch="last", recover_mode="prev_dir")
         fw = lpad.get_fw_by_id(fw_id)
-        fw.spec['_queueadapter'] = {'walltime': '06:00:00'}
-        fw.tasks[1]["other_params"]["user_incar_settings"].update({"ALGO":"All"})
+        # fw.spec['_queueadapter'] = {'walltime': '06:00:00'}
+        # fw.tasks[1]["other_params"]["user_incar_settings"].update({"ALGO":"All"})
         # fw.tasks[1]["incar_update"].update({"ALGO": "All"})
-        lpad.update_spec([fw.fw_id], fw.as_dict()["spec"])
-        # shutil.copy("CONTCAR", "POSCAR")
-        subprocess.call("qlaunch -c {} -r singleshot -f {}".format(
-            os.path.join(os.path.expanduser("~"), "config/project/Scan2dMat/calc_data/"), fw.fw_id).split(" "))
+        # lpad.update_spec([fw.fw_id], fw.as_dict()["spec"])
+        shutil.copy("CONTCAR", "POSCAR")
+        # subprocess.call("qlaunch -c {} -r singleshot -f {}".format(
+        #     os.path.join(os.path.expanduser("~"), "config/project/Scan2dDefect/calc_data/"), fw.fw_id).split(" "))
+    def rerun_scf(fw_id):
+        subprocess.call("gunzip INCAR.gz".split(" "))
+        incar = Incar.from_file("INCAR")
+        incar.update({"NELM": 200, "ALGO": "Fast"})
+        incar.write_file("INCAR")
+        lpad.rerun_fw(fw_id, recover_launch="last", recover_mode="prev_dir")
+
 
 
 
     fws = lpad.get_fw_ids(
         {
-            "state": {"$in": ["RUNNING"]}, #First running, then fizzled
-            "name": {"$regex": "SCAN_nscf line"},
+            "state": {"$in": ["READY"]}, #First running, then fizzled
+            "name": {"$regex": "static"},
             "spec._fworker": fworker,
-            # "fw_id": {"$nin": [1751, 1763, 2003, 2051]}
+            "created_on": {"$gte": str(datetime.datetime(2021,8,12))}
+            # "fw_id": {"$gte": 1797}
             # "fw_id": 2550
         }
     )
+    print(fws)
     a = []
-    for fw_id in fws:
+    for fw_id in fws[::-1][:]:
         prev_fw = lpad.get_fw_by_id(fw_id)
         fworker = prev_fw.spec["_fworker"]
-        prev_path = lpad.get_launchdir(fw_id)
-        # os.chdir(prev_path)
+        prev_path = lpad.get_launchdir(fw_id, 0)
+        os.chdir(prev_path)
         # a.append(fw_id)
         # if glob("CONTCAR.relax*") != []:
         try:
             print(fw_id, fworker, prev_path, prev_fw.name)
             # delet_dir(prev_path)
-            rerun_opt(fw_id)
+            # rerun_opt(fw_id)
+            # rerun_scf(fw_id)
         except Exception as err:
             print(err)
             continue
@@ -84,7 +95,7 @@ def rerun_relax(fworker):
         #     # rerun_opt(fw)
     print(a, len(a))
 
-rerun_relax("efrc")
+rerun_fw("efrc")
 
 #%%
 def rerun_irvsp():
@@ -112,9 +123,10 @@ import os
 lpad = LaunchPad.from_file(
     os.path.join(os.path.expanduser("~"), "config/project/Scan2dDefect/calc_data/my_launchpad.yaml"))
 for i in lpad.get_wf_ids({"state": "COMPLETED"}):
+    print(i)
     try:
         fw = lpad.get_fw_by_id(i)
-        if fw.spec["_fworker"] == "efrc":
+        if fw.spec["_fworker"] == "owls":
             print(i, fw.spec["_fworker"])
             lpad.delete_wf(i, True)
     except Exception:
@@ -131,6 +143,23 @@ for i in lpad.get_wf_ids({"state": "READY"})[:10]:
         if fw.spec["_fworker"] == "owls":
             fw.spec["_fworker"] = "efrc"
             lpad.update_spec([fw.fw_id], fw.as_dict()["spec"])
+
+
+#%%
+from fireworks import LaunchPad
+import os
+
+lpad = LaunchPad.from_file(
+    os.path.join(os.path.expanduser("~"), "config/project/Scan2dDefect/calc_data/my_launchpad.yaml"))
+for i in lpad.get_wf_ids({"state": "COMPLETED"}):
+    print(i)
+    try:
+        fw = lpad.get_fw_by_id(i)
+        if fw.spec["_fworker"] == "owls":
+            print(i, fw.spec["_fworker"])
+            lpad.delete_wf(i, True)
+    except Exception:
+        continue
 
 
 
